838e23ccb37b733bca45498da6319a8aed5fca21
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-05-28 07:01:34 -0700

    Add reverse.py

---
+++ reverse.py
@@ -0,0 +1,9 @@
+def reverse(text):
+  newText = list()
+  for num in range(len(text)):
+    print(text[len(text)-num-1])
+    newText.append(text[len(text)-num-1])
+    print ("".join(newText))
+  return("".join(newText))
+
+reverse("Derek")
