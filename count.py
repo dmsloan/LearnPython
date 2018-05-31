45bcdee03b84744eb944acfaedacb7684b5dec1e
Derek Sloan DerekHP2017 <dsloan@ibe-inc.com>
2018-05-30 17:20:15 -0700

    update count.py

--- count.py
+++ count.py
@@ -1,2 +1,9 @@
 def count(sequence, item):
-    
+    quan = 0
+    for num in sequence:
+        if num == item:
+            quan += 1
+    print(quan)
+    return(quan)
+
+count([1, 2, 1, 1, 1], 4)
