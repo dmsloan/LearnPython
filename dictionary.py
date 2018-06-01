27f47dabe96d0bb711678d4ed5c07d013b20e9b6
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-05-31 23:38:20 -0700

    add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "Name":"Ralph",
+    "Age":13,
+    "School":"Hale"
+}
+
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+
+for key in my_dict:
+    print(key, my_dict[key])
