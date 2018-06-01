49787235883e399013ba6e0fa626d4149f2b53d8
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-05-31 22:01:35 -0700

    Finished median.py added call.py

--- median.py
+++ median.py
@@ -1,17 +1,10 @@
-def mediana(numlist):
+def median(numlist):
     numListSort = sorted(numlist)
-    print(numlist)
-    print(numListSort)
-    print(numListSort[3])
+    med = None
     if len(numListSort) % 2 > 0:
-        print(numListSort[int(len(numListSort)/2-.5)])
+        med = (numListSort[int(len(numListSort)/2-.5)])
     else:
-        
-
-    for num in numlist:
-        if not num in noDup:
-            noDup.append(num)
-    print(noDup)
-    return(noDup)
-
-mediana([7, 12, 3, 1, 6])
+        med = ((numListSort[int(len(numListSort)/2)] + numListSort[int(len(numListSort)/2-1)])/2)
+    print(med)
+    return(med)
+median([4, 5, 5, 4])
