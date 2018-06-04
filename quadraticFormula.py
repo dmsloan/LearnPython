1a2d327dd698ed9618e6244d1042e7f31bd4afe1
Derek Sloan on A8 <dsloan@ibe-inc.com>
2018-06-03 23:44:01 -0700

    quadraticFormula works

--- quadraticFormula.py
+++ quadraticFormula.py
@@ -1,9 +1,10 @@
 def quad(a,b,c):
-    x = (4*a*c)
-    if b**2(-4*a*c) < 0:
+    underSquareRoot = (b**2-(4*a*c)) # you must put the * betwwen numbers to multiply, no 4ac
+    if underSquareRoot < 0:
         print("No real solution")
         return()
-
-quad(2,-1,-1)
-
-2V^2-v-1
+    else:
+        squareRoot = underSquareRoot**(1/2) #this line is underSquareRoot to the 
+                                            #1/2 power, its equal to the square root without loading a library
+        print("X=(",(-b+squareRoot)/(2*a),",",(-b-squareRoot)/(2*a),")")        
+quad(5,6,1)
